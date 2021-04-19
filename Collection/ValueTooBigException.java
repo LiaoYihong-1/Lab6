131edd61c8fcb36094aee4c822d311a3ef02d987
LiaoYihong-1 <74876152+LiaoYihong-1@users.noreply.github.com>
2021-04-19 17:45:28 +0300

    Add files via upload

---
+++ Collection/ValueTooBigException.java
@@ -0,0 +1,14 @@
+package Collection;
+
+/**
+ * exception ValueTooBigException,which will be thrown when set a value too big
+ */
+public class ValueTooBigException extends RuntimeException{
+    /**
+     * constructor
+     * @param message Message
+     */
+    public ValueTooBigException(String message){
+        super(message);
+    }
+}
