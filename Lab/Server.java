04ea76729322c234475bcda0e9154e5beb8e2c76
LiaoYihong-1 <74876152+LiaoYihong-1@users.noreply.github.com>
2021-04-28 13:24:11 +0300

    Update Server.java

--- Lab/Server.java
+++ Lab/Server.java
@@ -1,4 +1,6 @@
 package Lab;
+import CSV.CSVReader;
+import CSV.CSVWriter;
 import Collection.*;
 import Command.*;
 import java.io.*;
@@ -10,8 +12,10 @@ public class Server {
         //initialize collection of people
         new CollectionsofPerson().doInitialization();
         CommandManager manager = new CommandManager();
-        DatagramSocket datagramSocket = new DatagramSocket(5555);
+        int port = Integer.parseInt(args[0]);
+        DatagramSocket datagramSocket = new DatagramSocket(port);
 
+        boolean connect = true;
         while (true) {
             //receive
             byte[] buffer = new byte[102400];
@@ -21,15 +25,71 @@ public class Server {
             //main part
             try {
                 //deal with different command
-                runningcommand(inputStream,manager,datagramSocket,datagramPacket);
+                CommandPackage commandPackage = (CommandPackage) inputStream.readObject();
+                if (commandPackage != null) {
+                    if(commandPackage.isSet()&connect){
+                        LinkedHashSet<Person> people = new LinkedHashSet<>();
+                        File F = new File(commandPackage.getFileName());
+                        if(!F.exists()){
+                            F.createNewFile();
+                            new CSVWriter().writetofile(people,commandPackage.getFileName());
+                        }else {
+                            people = new CSVReader().readfile(people,commandPackage.getFileName());
+                        }
+                        System.out.printf("The port of client is %d\n",datagramPacket.getPort());
+                        CollectionsofPerson.setPeople(people);
+                        manager.setOut("Your collection is synchronized by file\n",false);
+                        connect = false;
+                        FirstResponse response = new FirstResponse(people,manager.getOut());
+                        ByteArrayOutputStream BAO = new ByteArrayOutputStream();
+                        ObjectOutputStream OOS = new ObjectOutputStream(BAO);
+                        OOS.writeObject(response);
+                        DatagramPacket send = new DatagramPacket(BAO.toByteArray(),0,BAO.toByteArray().length,datagramPacket.getSocketAddress());
+                        datagramSocket.send(send);
+                        System.out.print(manager.getOut());
+                        System.out.print("\n");
+                        OOS.close();
+                    } else if (commandPackage.getList()==null&!commandPackage.isSet()) {
+                        AbstractCommand command = commandPackage.getAbstractCommand();
+                        new History().getHistory().add(command.getName() + "\n");
+                        if (command.getName().equalsIgnoreCase("exit")) {
+                            command.execute(manager,commandPackage);
+                            datagramSocket.send(new DatagramPacket(manager.getOut().getBytes(), manager.getOut().getBytes().length, datagramPacket.getSocketAddress()));
+                            new Save().execute(manager, commandPackage);
+                            System.exit(2);
+                        } else {
+                            command.execute(manager, commandPackage);
+                            if (command.getName().equalsIgnoreCase("updateid")) {
+                                CollectionsofPerson.setPeople(sort(new CollectionsofPerson().getPeople()));
+                            }
+                        }
+                        byte [] data = manager.getOut().getBytes();
+                        DatagramPacket send = new DatagramPacket(data,data.length,datagramPacket.getSocketAddress());
+                        datagramSocket.send(send);
+                        System.out.print(manager.getOut());
+                        System.out.print("\n");
+                    }else if(!commandPackage.isSet()){
+                        StringBuilder S = new StringBuilder();
+                        List<CommandPackage> list = commandPackage.getList();
+                        Iterator<CommandPackage> iterator = list.iterator();
+                        while(iterator.hasNext()){
+                            CommandPackage used = iterator.next();
+                            AbstractCommand command = used.getAbstractCommand();
+                            new History().getHistory().add(command.getName()+"\n");
+                            command.execute(manager,used);
+                            S.append(manager.getOut()).append("\n");
+                        }
+                        manager.setOut(S.toString(),false);
+                        byte [] data = manager.getOut().getBytes();
+                        DatagramPacket send = new DatagramPacket(data,data.length,datagramPacket.getSocketAddress());
+                        datagramSocket.send(send);
+                        System.out.print(manager.getOut());
+                        System.out.print("\n");
+                    }
+                }
             }catch (ClassNotFoundException|ParaInapproException|NullException C) {
                 manager.setOut(C.getMessage(),false);
             }
-
-            //send
-            byte [] data = manager.getOut().getBytes();
-            DatagramPacket send = new DatagramPacket(data,data.length,datagramPacket.getSocketAddress());
-            datagramSocket.send(send);
         }
     }
 
@@ -39,41 +99,5 @@ public class Server {
         comparator = Comparator.comparingInt(a -> a.getId());
         linkedHashSet.stream().sorted(comparator).forEach(newone::add);
         return newone;
-    }
-
-    public static void runningcommand(ObjectInputStream inputStream,CommandManager manager,DatagramSocket datagramSocket,DatagramPacket datagramPacket)throws ClassNotFoundException,NullException,IOException{
-        CommandPackage commandPackage = (CommandPackage) inputStream.readObject();
-        if (commandPackage != null) {
-            if(commandPackage.isSet()){
-                CollectionsofPerson.setPeople(commandPackage.getLinkedHashSet());
-                manager.setOut("Your collection is synchronized by file\n",false);
-            } else if (commandPackage.getList()==null) {
-                AbstractCommand command = commandPackage.getAbstractCommand();
-                new History().getHistory().add(command.getName() + "\n");
-                if (command.getName().equalsIgnoreCase("exit")) {
-                    command.execute(manager,commandPackage);
-                    datagramSocket.send(new DatagramPacket(manager.getOut().getBytes(), manager.getOut().getBytes().length, datagramPacket.getSocketAddress()));
-                    new Save().execute(manager, commandPackage);
-                    System.exit(2);
-                } else {
-                    command.execute(manager, commandPackage);
-                    if (command.getName().equalsIgnoreCase("updateid")) {
-                        CollectionsofPerson.setPeople(sort(new CollectionsofPerson().getPeople()));
-                    }
-                }
-            }else{
-                String S = "";
-                List<CommandPackage> list = commandPackage.getList();
-                Iterator<CommandPackage> iterator = list.iterator();
-                while(iterator.hasNext()){
-                    CommandPackage used = iterator.next();
-                    AbstractCommand command = used.getAbstractCommand();
-                    new History().getHistory().add(command.getName()+"\n");
-                    command.execute(manager,used);
-                    S=S+manager.getOut()+"\n";
-                }
-                manager.setOut(S,false);
-            }
-        }
     }
 }
